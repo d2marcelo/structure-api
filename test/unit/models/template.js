c0dcb6316251c6d4671120e508496176f50872dc
Todd Hamilton <toddkh@gmail.com>
2016-07-25 12:42:20 -0400

    added test: template revision update

--- test/unit/models/template.js
+++ test/unit/models/template.js
@@ -79,7 +79,54 @@ describe('Template', function() {
 
   })
 
-  it('should update a template revision', async function(done) {
+  it('should update a template revision',  async function(done) {
+
+    var template = new Template({
+      name: 'templates'
+    })
+
+    var res = await template.create({
+      title: 'Fun Template'
+    })
+
+    var revision = new Revision({
+      templateId: res.id
+    })
+
+    var res2 = await revision.create({
+      fields: [
+        {
+          fieldType: 'text-input',
+          title: 'Document Title'
+        }
+      ],
+      title: 'Fun Template 2'
+    })
+
+    var res3 = await template.update(res.id, {
+      activeRevisionId: res2.id,
+      revisionIds: [res2.id]
+    })
+
+    var res4 = await revision.update(res2.id, {
+      fields: [
+        {
+          fieldType: 'text-input',
+          title: 'Revision Title'
+        },
+        {
+          fieldType: 'text-input',
+          title: 'Description'
+        }
+      ],
+      title: 'Fun Template 3'
+    })
+
+    expect(res4.fields.length).to.equal(2)
+    expect(res4.fields[0].title).to.equal('Revision Title')
+    expect(res4.fields[1].title).to.equal('Description')
+
+    done()
 
 
 
