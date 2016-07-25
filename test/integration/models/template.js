8571c0c53819a79dfc9041a407ebfdeed4f21f46
Chris Abrams <mail@chrisabrams.com>
2016-07-25 11:57:01 -0400

    remove this

--- test/integration/models/template.js
+++ test/integration/models/template.js
@@ -8,15 +8,7 @@ describe('Template', function() {
 
   it('should create a template',  async (done) => {
 
-    var template = new Template({
-      name: 'templates'
-    })
 
-    var res = await template.create({
-      title: 'Fun Template'
-    })
-
-    expect(res.title).to.equal('Fun Template')
 
     done()
 
@@ -24,22 +16,7 @@ describe('Template', function() {
 
   it('should create a template revision',  async (done) => {
 
-    var template = new Template({
-      name: 'templates'
-    })
-
-    var res = await template.create({
-      title: 'Fun Template'
-    })
-
-    var revision = new Revision({
-
-    })
-
-    // here
-
-    expect(res2.fields.length).to.equal(1)
-    expect(res2.fields[0].title).to.equal('Document Title')
+    
 
     done()
 
