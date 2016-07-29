08a677d0be473e63abcb2e49e09e54f6fd1ce651
juan gabriel ramirez <tuxgabo@gmail.com>
2016-07-28 20:05:55 -0500

    template and template revision test added

--- test/unit/models/template.js
+++ test/unit/models/template.js
@@ -31,6 +31,58 @@ describe('Template', function() {
 
   })
 
+  /** @test {TemplateModel#getByID} */
+  it('should get a template by ID', async function(done){
+
+    var template = new  TemplateModel()
+
+    var templateRes = await template.create({
+      title: 'Fun Template'
+    })
+
+    var getByIdResponse = await  template.getById(templateRes.id)
+
+    expect(getByIdResponse.title).to.equal('Fun Template')
+
+    done()
+
+  })
+
+  /** @test {TemplateModel#getAll} **/ 
+  it('should get all templates', async function(done){
+
+    var template = new  TemplateModel()
+
+    var templateRes = await template.create({
+      title: 'Fun Template'
+    })
+
+    var getAllResponse = await template.getAll();
+    expect(getAllResponse.length > 0).to.be.true
+
+    done()
+
+  })
+
+  /** @test {TemplateModel#update} */ 
+  it('should update a template ', async function(done) {
+
+    var  template = new TemplateModel()
+
+    var templateRes = await template.create({
+      title: 'Fun Template'
+    })
+
+    var templateUpdateRes= await template.update(templateRes.id, { 
+      title: 'Fun Template 3'
+    })
+
+    expect(templateUpdateRes.title).to.equal('Fun Template 3')
+
+    done()
+
+  })
+
   /** @test {TemplateRevisionModel#create} */
   it('should create a template revision',  async function(done) {
 
@@ -46,10 +98,10 @@ describe('Template', function() {
 
     var res2 = await revision.create({
       fields: [
-        {
-          fieldType: 'text-input',
-          title: 'Document Title'
-        }
+      {
+        fieldType: 'text-input',
+        title: 'Document Title'
+      }
       ]
     })
 
@@ -63,6 +115,68 @@ describe('Template', function() {
     expect(res2.fields[0].title).to.equal('Document Title')
     expect(res3.activeRevisionId).to.equal(res2.id)
     expect(res3.title).to.equal('Fun Template 2')
+
+    done()
+
+  })
+
+  /** @test {TemplateModel#getByID} */
+  it('should get a template by ID', async function(done){
+
+    var template = new  TemplateModel()
+
+    var templateRes = await template.create({
+      title: 'Fun Template'
+    })
+
+    var revision = new TemplateRevisionModel({
+      templateId: templateRes.id
+    })
+
+    var revisionRes = await revision.create({
+      fields: [
+      {
+        fieldType: 'text-input',
+        title: 'Document Title'
+      }
+      ]
+    })
+
+    var getByIdRes = await  revision.getById(revisionRes.id)
+
+
+    expect(getByIdRes.fields.length).to.equal(1)
+    expect(getByIdRes.fields[0].title).to.equal('Document Title')
+
+    done()
+
+  })
+
+  /** @test {TemplateModel#getAll} **/ 
+  it('should get all templates revisions', async function(done){
+
+    var template = new TemplateModel()
+
+    var templateRes = await template.create({
+      title: 'Fun Template'
+    })
+
+    var revision = new TemplateRevisionModel({
+      templateId: templateRes.id
+    })
+
+    var revisionRes = await revision.create({
+      fields: [
+      {
+        fieldType: 'text-input',
+        title: 'Document Title'
+      }
+      ]
+    })
+
+    var getAllResponse = await revision.getAll();
+
+    expect(getAllResponse.length > 0).to.be.true
 
     done()
 
@@ -83,10 +197,10 @@ describe('Template', function() {
 
     var res2 = await revision.create({
       fields: [
-        {
-          fieldType: 'text-input',
-          title: 'Document Title'
-        }
+      {
+        fieldType: 'text-input',
+        title: 'Document Title'
+      }
       ]
     })
 
@@ -98,14 +212,14 @@ describe('Template', function() {
 
     var res4 = await revision.update(res2.id, {
       fields: [
-        {
-          fieldType: 'text-input',
-          title: 'Revision Title'
-        },
-        {
-          fieldType: 'text-input',
-          title: 'Description'
-        }
+      {
+        fieldType: 'text-input',
+        title: 'Revision Title'
+      },
+      {
+        fieldType: 'text-input',
+        title: 'Description'
+      }
       ]
     })
 
